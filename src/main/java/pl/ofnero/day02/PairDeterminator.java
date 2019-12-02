00ad2649c3d50336e239cd9763b0c4f39c11b020
Maciej Faron <maciejfaron@o2.pl>
2019-12-02 18:46:49 +0100

    Do some refactoring

--- src/main/java/pl/ofnero/day02/PairDeterminator.java
+++ src/main/java/pl/ofnero/day02/PairDeterminator.java
@@ -2,13 +2,14 @@ package pl.ofnero.day02;
 
 public class PairDeterminator {
     public static int findVerbAndNoun(int expectedOutput, String fileName) {
+        int[] array = PositionChanger.loadCode(fileName);
         for (int noun = 0; noun <= 99; noun++) {
             for (int verb = 0; verb <= 99; verb++) {
-                int[] array = PositionChanger.loadCode(fileName);
-                array[1] = noun;
-                array[2] = verb;
-                PositionChanger.process(array);
-                if (array[0] == expectedOutput) {
+                int[] temp = array.clone();
+                temp[1] = noun;
+                temp[2] = verb;
+                PositionChanger.process(temp);
+                if (temp[0] == expectedOutput) {
                     return 100 * noun + verb;
                 }
             }
