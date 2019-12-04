d9abc7e9216b7abab7065c36eaa6a0dc3a5ac0f4
Maciej Faron <maciejfaron@o2.pl>
2019-12-04 10:52:13 +0100

    Refactor

--- src/main/java/pl/ofnero/day04/Main.java
+++ src/main/java/pl/ofnero/day04/Main.java
@@ -3,10 +3,13 @@ package pl.ofnero.day04;
 import java.util.function.IntPredicate;
 
 public class Main {
+    private static final int START = 246515;
+    private static final int END = 739105;
+    
     public static void main(String[] args) {
         IntPredicate firstPart = PasswordValidator::validate;
         IntPredicate secondPart = PasswordValidator::validatePartTwo;
-        System.out.println(PasswordValidator.countGoodPasswordWithinRange(246515, 739105, firstPart));
-        System.out.println(PasswordValidator.countGoodPasswordWithinRange(246515, 739105, secondPart));
+        System.out.println(PasswordValidator.countGoodPasswordWithinRange(START, END, firstPart));
+        System.out.println(PasswordValidator.countGoodPasswordWithinRange(START, END, secondPart));
     }
 }
